d15b059190375b50639efc99c42849f66939945a
Enzo Li <Eynzof@gmail.com>
2022-10-14 19:10:09 +0800

    register page update

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,38 +1,49 @@
 import React from "react";
 import { Field, Form, Formik } from "formik";
 import {
+  Box,
+  Button,
   FormControl,
   FormErrorMessage,
   FormLabel,
   Input,
 } from "@chakra-ui/react";
 import { Wrapper } from "../components/Wrapper";
+import { InputField } from "../components/InputField";
 
 interface registerProps {}
 
 export const Register: React.FC<registerProps> = ({}) => {
   return (
     <Wrapper>
-    <Formik
-      initialValues={{ username: "", password: "" }}
-      onSubmit={(values) => {
-        console.log(values);
-      }}>
-      {({ values, handleChange }) => (
-        <Form>
-          <FormControl>
-            <FormLabel htmlFor="username">Username</FormLabel>
-            <Input
-              value={values.username}
-              onChange={handleChange}
-              id="username"
-              placeholder="name"
-            />
-            {/* <FormErrorMessage>{form.errors.name}</FormErrorMessage> */}
-          </FormControl>
-        </Form>
-      )}
-    </Formik>
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}>
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
+              placeholder="username"
+              label="Username"></InputField>
+            <Box mt="4">
+              <InputField
+                name="password"
+                placeholder="password"
+                label="Password"
+                type="password"></InputField>
+            </Box>
+            <Button
+              mt={4}
+              type="submit"
+              isLoading={isSubmitting}
+              colorScheme="teal">
+              register
+            </Button>
+          </Form>
+        )}
+      </Formik>
     </Wrapper>
   );
 };
