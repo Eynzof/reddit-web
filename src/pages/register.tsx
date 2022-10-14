c548a3db6bb678293beeca44743223c81b33bbf2
Enzo Li <Eynzof@gmail.com>
2022-10-15 00:53:33 +0800

    setup codegen

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -12,29 +12,24 @@ import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
 import { useMutation } from "urql";
 
+// import { useRegisterMutation } from "../../../reddit/src/gql/index";
+import { useRegisterMutation } from "../gql/graphql";
+
 interface registerProps {}
 
-const REGISTER_MUT = `mutation Register($username: String!, $password:String!) {
-  register(options: { username: $username, password: $password }) {
-    errors {
-      field
-      message
-    }
-    user {
-      username
-    }
-  }
-}
-`;
 
 export const Register: React.FC<registerProps> = ({}) => {
-  const [, register] = useMutation(REGISTER_MUT);
+  const [, register] = useRegisterMutation();
   return (
     <Wrapper>
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={(values) => {
-          register(values); 
+        onSubmit={async (values, {setErrors}) => {
+          const response = await register(values); 
+          if (response.data?.register.errors) {
+          
+            setErrors({username:"I'm an error"})
+          }
         }}>
         {({ isSubmitting }) => (
           <Form>
