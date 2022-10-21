4ff4c6379c2cfe99f2bb1d14e0a3d93d49c23059
enzo <Eynzof@gmail.com>
2022-10-21 16:15:58 +0800

    install package

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,22 @@
-import { NavBar } from "../components/NavBar";
+import { NavBar } from '../components/NavBar';
+import { withUrqlClient } from 'next-urql';
+import { createUrqlClient } from '../utils/createUrqlClient';
+import { usePostsQuery } from '../gql/graphql';
 
-const Index = () => (
-  <>
-    <NavBar />
-    <div>hello</div>
-  </>
-);
+const Index = () => {
+  const [{ data }] = usePostsQuery();
+  return (
+    <>
+      <NavBar />
+      <div>hello world</div>
+      <br />
+      {!data ? (
+        <div>loading...</div>
+      ) : (
+        data.posts.map((p) => <div key={p.id}>{p.title}</div>)
+      )}
+    </>
+  );
+};
 
-export default Index;
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
