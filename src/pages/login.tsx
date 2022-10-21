fa1408795d54a9a05478f532e70a6c4402892db0
enzo <Eynzof@gmail.com>
2022-10-21 11:52:31 +0800

    fixed cookie issues

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,57 +1,51 @@
-import React from "react";
-import { Field, Form, Formik } from "formik";
-import {
-  Box,
-  Button,
-  FormControl,
-  FormErrorMessage,
-  FormLabel,
-  Input,
-} from "@chakra-ui/react";
-import { Wrapper } from "../components/Wrapper";
-import { InputField } from "../components/InputField";
-import { useMutation } from "urql";
+import React from 'react';
+import { Form, Formik } from 'formik';
+import { Box, Button } from '@chakra-ui/react';
+import { Wrapper } from '../components/Wrapper';
+import { InputField } from '../components/InputField';
 
-// import { useRegisterMutation } from "../../../reddit/src/gql/index";
-import { useLoginMutation } from "../gql/graphql";
-import { toErrorMap } from "../utils/toErrorMap";
-import { useRouter } from "next/router";
+import { useLoginMutation } from '../gql/graphql';
+import { toErrorMap } from '../utils/toErrorMap';
+import { useRouter } from 'next/router';
 
 export const Login: React.FC<{}> = ({}) => {
   const router = useRouter();
   const [, login] = useLoginMutation();
   return (
-    <Wrapper>
+    <Wrapper variant="small">
       <Formik
-        initialValues={{ username: "", password: "" }}
+        initialValues={{ username: '', password: '' }}
         onSubmit={async (values, { setErrors }) => {
           const response = await login({ options: values });
           if (response.data?.login.errors) {
             setErrors(toErrorMap(response.data.login.errors));
           } else if (response.data?.login.user) {
             // worked
-            console.log(response.data)
-            router.push("/");
+            router.push('/');
           }
-        }}>
+        }}
+      >
         {({ isSubmitting }) => (
           <Form>
             <InputField
               name="username"
               placeholder="username"
-              label="Username"></InputField>
+              label="Username"
+            ></InputField>
             <Box mt="4">
               <InputField
                 name="password"
                 placeholder="password"
                 label="Password"
-                type="password"></InputField>
+                type="password"
+              ></InputField>
             </Box>
             <Button
               mt={4}
               type="submit"
               isLoading={isSubmitting}
-              colorScheme="teal">
+              colorScheme="teal"
+            >
               login
             </Button>
           </Form>
