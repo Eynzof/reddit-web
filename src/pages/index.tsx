b36b9c7844318af42b1818a6b9e6cfa288b03d5c
enzo <Eynzof@gmail.com>
2022-10-27 14:51:03 +0800

    posts style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,32 @@
-import { NavBar } from '../components/NavBar';
-import { withUrqlClient } from 'next-urql';
-import { createUrqlClient } from '../utils/createUrqlClient';
-import { usePostsQuery } from '../gql/graphql';
-import { Layout } from '../components/Layout';
-import NextLink from 'next/link';
-import { Link } from '@chakra-ui/react';
+import { withUrqlClient } from "next-urql";
+import { createUrqlClient } from "../utils/createUrqlClient";
+import { usePostsQuery } from "../gql/graphql";
+import { Layout } from "../components/Layout";
+import NextLink from "next/link";
+import { Box, Heading, Link, Stack } from "@chakra-ui/react";
+import { Text } from "@chakra-ui/react";
 const Index = () => {
-  const [{ data }] = usePostsQuery();
+  const [{ data }] = usePostsQuery({
+    variables: {
+      limit: 10,
+    },
+  });
   return (
     <Layout>
-      <NextLink href={'/create-post'}>
+      <NextLink href={"/create-post"}>
         <Link>create post</Link>
       </NextLink>
       {!data ? (
         <div>loading...</div>
       ) : (
-        data.posts.map((p) => <div key={p.id}>{p.title}</div>)
+        <Stack spacing={8}>
+          {data.posts.map((p) => (
+            <Box key={p.id} p={5} shadow="md" borderWidth="1px">
+              <Heading fontSize="xl">{p.title}</Heading>
+              <Text mt={4}>{p.text}</Text>
+            </Box>
+          ))}
+        </Stack>
       )}
     </Layout>
   );
