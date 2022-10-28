29d4606ad35543f985840800ab18a98d88d88ac6
enzo <Eynzof@gmail.com>
2022-10-28 23:18:31 +0800

    block duplicate vote

--- src/components/UpdootSection.tsx
+++ src/components/UpdootSection.tsx
@@ -2,6 +2,7 @@ import { ChevronDownIcon, ChevronUpIcon } from '@chakra-ui/icons';
 import { Flex, IconButton } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import { PostSnippetFragment, useVoteMutation } from '../gql/graphql';
+import { useToast } from '@chakra-ui/react';
 
 interface UpdootSectionProps {
   post: PostSnippetFragment;
@@ -10,6 +11,7 @@ export const UpdootSection: React.FC<UpdootSectionProps> = ({ post }) => {
   const [loadingState, setLoadingState] = useState<
     'updoot-loading' | 'downdoot-loading' | 'not-loading'
   >('not-loading');
+  const toast = useToast();
   // useVoteMutation here
   const [, vote] = useVoteMutation();
   return (
@@ -25,6 +27,22 @@ export const UpdootSection: React.FC<UpdootSectionProps> = ({ post }) => {
         w={6}
         h={6}
         onClick={async () => {
+          // has voted up so nothing happen
+          if (post.voteStatus === 1) {
+            console.log('there should be a toast');
+            {
+              () => {
+                toast({
+                  title: `already voted up`,
+                  status: 'error',
+                  duration: 9000,
+                  isClosable: true,
+                });
+              };
+            }
+            return;
+          }
+
           setLoadingState('updoot-loading');
           await vote({
             postId: post.id,
@@ -44,6 +62,18 @@ export const UpdootSection: React.FC<UpdootSectionProps> = ({ post }) => {
         h={6}
         background={post.voteStatus === -1 ? 'red' : 'null'}
         onClick={async () => {
+          // has voted up so nothing happen
+          if (post.voteStatus === -1) {
+            console.log('there should be a toast');
+            toast({
+              title: `already voted down`,
+              description: "We've created your account for you.",
+              status: 'error',
+              duration: 9000,
+              isClosable: true,
+            });
+            return;
+          }
           setLoadingState('updoot-loading');
           await vote({
             postId: post.id,
